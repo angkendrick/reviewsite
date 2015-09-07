d5dc810b856fb3070e115094fc328e208d444519
Ken Ang <angkendrick@gmail.com>
2015-09-07 17:46:16 -0400

    updated reviews controller render

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,23 +17,23 @@ class ReviewsController < ApplicationController
     @review.restaurant_id = @restaurant.id
 
     if @review.save
-      format.html { redirect_to @restaurant, notice: 'Review was successfully created.' }
+      redirect_to @restaurant, notice: 'Review was successfully created.'
     else
-      format.html { render :new }
+      render :new
     end
   end
 
   def update
     if @review.update(review_params)
-      format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+      redirect_to @review, notice: 'Review was successfully updated.'
     else
-      format.html { render :edit }
+      render :edit
     end
   end
 
   def destroy
     @review.destroy
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      redirect_to reviews_url, notice: 'Review was successfully destroyed.'
   end
 
   private
